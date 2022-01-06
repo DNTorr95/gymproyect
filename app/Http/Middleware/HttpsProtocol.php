98664c24e6603f3e64d5cf11f3d766fa0efd4aba
DNTorr95 <dussantorres22@gmail.com>
2022-01-05 21:36:31 -0400

    se corrigieron errores

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -18,8 +18,7 @@ class HttpsProtocol
     public function handle(Request $request, Closure $next)
     {   
         if(!$request->secure()){ 
-            return redirect()
-            ->secure($request->getRequestUri());
+            return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
     }
