a63dccfd4b8e0aad9cba5baa8ae1ae52ad49fb2d
DNTorr95 <dussantorres22@gmail.com>
2022-01-05 21:01:03 -0400

    se creo un @csrf

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -256,7 +256,7 @@
                                 </a>-->
                                 <!--<div class="dropdown-divider"></div>-->
                                 <form action="{{route('logout')}}" method="POST">
-                                    
+                                    {{ csrf_field() }}
                                 <button class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Salir
